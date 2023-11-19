d26c5ce5d1b833dab869622e24d8aba49c78d2c5
tanay-clickzy <rocks.d.xebec2411@gmail.com>
2023-11-19 14:43:15 +0530

    comit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Roultte Game',
+      title: 'Roulette Game',
       theme: ThemeData(
         useMaterial3: true,
         primaryColor: Colors.orange, 
